f85b6460a59f2562352b366219258da688a126c5
Frank Angelo Malubag <frankabad189@gmail.com>
2026-02-04 16:19:51 +0800

    mounted /orders path and its route handler orderRoutes with complete Postman testing for creating, paying, shipping, and completing the order completely functional.

--- src/app.js
+++ src/app.js
@@ -3,4 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+// Import routes
+const orderRoutes = require("./routes/orders");
+
+// Mount routes
+app.use("/orders", orderRoutes);
+
 module.exports = app;
